9cdc679bc2c62575b4b0cc0ddfee319a323c1ea8
baka <denisfurdilov@gmail.com>
2024-06-03 15:28:48 +0700

    add logout

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth import update_session_auth_hash
 
 from rest_framework import status
@@ -19,7 +19,7 @@ from .serializers import *
 def get_csrf_token(request):
     return JsonResponse({'csrftoken': get_token(request)})
 
-@method_decorator(csrf_exempt, name='dispatch')
+#@method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(APIView):
     serializer_class = RegisterSerializer
     def post(self, request, *args, **kwargs):
@@ -29,7 +29,7 @@ class RegisterView(APIView):
             return Response({'detail': 'User registered successfully'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@method_decorator(csrf_exempt, name='dispatch')
+#@method_decorator(csrf_exempt, name='dispatch')
 class LoginView(APIView):
     serializer_class = LoginSerializer
 
@@ -94,3 +94,9 @@ class UpdateUserView(APIView):
             return Response(response_serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        logout(request)
+        return Response({"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
