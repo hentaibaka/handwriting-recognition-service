68c21eb1e050e7790b0368d21acc05bc2bffe273
Alex <unterberg2012@gmail.com>
2024-06-10 00:27:11 +0700

    add sage module

--- backend/recognition_module/recognize_function.py
+++ backend/recognition_module/recognize_function.py
@@ -5,6 +5,8 @@ from easyocr.easyocr import Reader
 from easyocr.trainer.utils import AttnLabelConverter
 import torch
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from sage.spelling_correction import AvailableCorrectors
+from sage.spelling_correction import RuM2M100ModelForSpellingCorrection
 
 
 def recognize_text_from_images(image_pieces, models_directory, recog_network='best_accuracy', gpu=False):
@@ -27,6 +29,8 @@ def recognize_text_from_images(image_pieces, models_directory, recog_network='be
     reader = Reader(['ru'], recog_network=recog_network, gpu=gpu,
                             model_storage_directory=model_storage_directory,
                             user_network_directory=user_network_directory)
+    # Подключение модели m2m для постобработки текста
+    corrector_m2m = RuM2M100ModelForSpellingCorrection.from_pretrained(AvailableCorrectors.m2m100_418M.value)
 
     recognized_texts = []
     for image_piece in image_pieces:
@@ -37,6 +41,8 @@ def recognize_text_from_images(image_pieces, models_directory, recog_network='be
             result = reader.readtext(image_cv, detail=0, decoder="beamsearch")
         else:
             result = reader.readtext(image_cv, detail=0)
+        #Постобработка текста с помощью модели m2m
+        result = corrector_m2m.correct(result)
         recognized_texts.append(" ".join(result))
     
     return recognized_texts
@@ -77,7 +83,8 @@ def recognize_text_from_imagesTrOCR(image_pieces, models_directory, gpu=False):
     processor = TrOCRProcessor.from_pretrained(models_directory)
     model = VisionEncoderDecoderModel.from_pretrained(models_directory)
     model.to(device)
-    # Initialize EasyOCR readers
+    # Подключение модели m2m для постобработки текста
+    corrector_m2m = RuM2M100ModelForSpellingCorrection.from_pretrained(AvailableCorrectors.m2m100_418M.value)
 
     recognized_texts = []
     for image_piece in image_pieces:
@@ -87,6 +94,8 @@ def recognize_text_from_imagesTrOCR(image_pieces, models_directory, gpu=False):
         pixel_values = processor(images=image_cv, return_tensors="pt").pixel_values
         generated_ids = model.generate(pixel_values)
         generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        # Постобработка текста с помощью модели m2m
+        generated_text = corrector_m2m.correct(generated_text)
         recognized_texts.append(generated_text)
     
     return recognized_texts
