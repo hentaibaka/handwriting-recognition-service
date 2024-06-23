32aaca6285978b3ef0c9799945af6767c421cd74
Denis Furdilov <61946499+hentaibaka@users.noreply.github.com>
2024-06-24 01:11:59 +0700

    Update CopyTextButton.tsx

--- frontend/src/pages/recognition/components/CopyTextButton.tsx
+++ frontend/src/pages/recognition/components/CopyTextButton.tsx
@@ -1,15 +1,29 @@
+import React, { useState } from "react";
 import { Button } from "@/components";
 
-interface CopyTextButton {
+interface CopyTextButtonProps {
   text: string | null;
 }
 
-export const CopyTextButton = ({ text }: CopyTextButton) => {
+export const CopyTextButton = ({ text }: CopyTextButtonProps) => {
+  const [buttonText, setButtonText] = useState("Скопировать");
+  const [isCopied, setIsCopied] = useState(false);
 
   const handleCopyText = async () => {
     if (!text) return;
 
-    
+    try {
+      await navigator.clipboard.writeText(text);
+      setButtonText("Скопировано");
+      setIsCopied(true);
+      setTimeout(() => {
+        setButtonText("Скопировать");
+        setIsCopied(false);
+      }, 2000); // Возвращаем исходный текст через 2 секунды
+    } catch (err) {
+      console.error("Ошибка при копировании текста: ", err);
+      alert("Не удалось скопировать текст");
+    }
   };
 
   return (
@@ -18,7 +32,7 @@ export const CopyTextButton = ({ text }: CopyTextButton) => {
       disabled={!text}
       onClick={handleCopyText}
     >
-      Скопировать
+      {buttonText}
     </Button>
   );
 };
