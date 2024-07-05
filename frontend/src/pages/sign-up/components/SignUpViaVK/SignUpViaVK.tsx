a5bc677ca9f27f6e04187e8b400087e802b2173b
baka <denisfurdilov@gmail.com>
2024-07-06 01:36:40 +0700

    add vk auth

--- frontend/src/pages/sign-up/components/SignUpViaVK/SignUpViaVK.tsx
+++ frontend/src/pages/sign-up/components/SignUpViaVK/SignUpViaVK.tsx
@@ -1,9 +1,15 @@
 import { Button, Icon } from "@/components";
 import { cn } from "@/lib/helpers";
 
+const vkAuthUrl = `https://oauth.vk.com/authorize?client_id=51986500&redirect_uri=https://manuscript.sfu-kras.ru/auth/complete/vk/&response_type=code&scope=email`;
+
 export const SignUpViaVK = (
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
+  const handleVKLogin = () => {
+    window.location.href = vkAuthUrl;
+  };
+
   return (
     <Button
       type="button"
@@ -11,6 +17,7 @@ export const SignUpViaVK = (
         "bg-additional-color-1 text-accent hover:bg-additional-color-1",
         "disabled:bg-additional-color-1/80 disabled:text-accent/60"
       )}
+      onClick={handleVKLogin}
       {...props}
     >
       <Icon
